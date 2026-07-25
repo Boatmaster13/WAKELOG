6c1a436c195518a564c321cc8402a16966ef539a
WAKELOG <ckf_13@outlook.com>
2026-07-25 12:43:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -25,7 +25,25 @@ const PRECACHE_FILES = [
   './sounds/erfinder.m4a',
   './sounds/richi.m4a',
   './sounds/sautubel.m4a',
-  './sounds/heb-din-schlitte.m4a'
+  './sounds/heb-din-schlitte.m4a',
+  // Jingle-Paket 2 (bereits geliefert)
+  './sounds/ach-leck-mich-doch.m4a', './sounds/du-dummer-dreckswichser.m4a', './sounds/du-arschloch.m4a',
+  './sounds/bist-du-komplett-durchgedreht.m4a', './sounds/du-opfer.m4a', './sounds/endlich-freitag.m4a',
+  './sounds/fuck.m4a', './sounds/furz.m4a', './sounds/du-bloede-sau.m4a', './sounds/handlungsbedarf.m4a',
+  './sounds/kein-widerspruch.m4a', './sounds/ja-scheisse.m4a', './sounds/arschloch.m4a', './sounds/fickschnitzel.m4a',
+  './sounds/mahlzeit.m4a', './sounds/laeuft.m4a', './sounds/change-request.m4a', './sounds/applaus.m4a',
+  './sounds/kaffee.m4a', './sounds/leck-mich-am-arsch.m4a',
+  // Noch ausstehende Dateien: schlagen beim Precache einfach fehl (wird übersprungen)
+  // und werden automatisch gecacht, sobald sie im Repo liegen und einmal gespielt wurden.
+  './sounds/restricted-area.m4a', './sounds/was-kannst-du-eigentlich.m4a', './sounds/oh-mann.m4a',
+  './sounds/zonk.m4a', './sounds/was-stimmt-bi-dier-noed.m4a', './sounds/morgen.m4a', './sounds/pfff.m4a',
+  './sounds/nein-nein-nein.m4a', './sounds/negativ.m4a', './sounds/schoen-aber-nein.m4a', './sounds/och-nee.m4a',
+  './sounds/mhhh-noe.m4a', './sounds/scheissegal.m4a', './sounds/zuviel-geld.m4a',
+  './sounds/nicht-meine-kostenstelle.m4a', './sounds/kuemmer-mich-drum.m4a',
+  './sounds/no-1.m4a', './sounds/no-2.m4a', './sounds/no-3.m4a', './sounds/no-4.m4a', './sounds/no-5.m4a',
+  './sounds/no-6.m4a', './sounds/no-7.m4a', './sounds/no-8.m4a', './sounds/no-9.m4a', './sounds/no-10.m4a',
+  './sounds/yes-1.m4a', './sounds/yes-2.m4a', './sounds/yes-3.m4a', './sounds/yes-4.m4a', './sounds/yes-5.m4a',
+  './sounds/yes-6.m4a', './sounds/yes-7.m4a', './sounds/yes-8.m4a', './sounds/yes-9.m4a', './sounds/yes-10.m4a'
 ];
 
 self.addEventListener('install', (event) => {
